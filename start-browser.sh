1d0606d6253b05d969235869f0e2601472862b37
Lukas Trumm <lukas.trumm@gmail.com>
2025-10-21 11:07:56 +0200

    chore

--- start-browser.sh
+++ start-browser.sh
@@ -3,7 +3,4 @@
 # Start Chrome with remote debugging in background
 google-chrome --remote-debugging-port=9222 --user-data-dir=/tmp/chrome-profile-stable &
 
-# Wait for Chrome to start
-sleep 2
-
 echo "Chrome started with remote debugging on port 9222"
